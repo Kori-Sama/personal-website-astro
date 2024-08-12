c3895d210cbdca8004f52d225589e5a708718f8d
Kori Miyohashi <Miyohashikori457@gmail.com>
2024-08-12 12:52:59 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,37 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 import playformCompress from "@playform/compress";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 import pagefind from "astro-pagefind";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://frosti.saroprock.com',
+  site: "https://frosti.saroprock.com",
   style: {
     scss: {
-      includePaths: ['./src/styles']
-    }
+      includePaths: ["./src/styles"],
+    },
   },
-  integrations: [mdx(), icon(), sitemap(), tailwind(), playformCompress(), pagefind()],
+  integrations: [
+    mdx(),
+    icon(),
+    sitemap(),
+    tailwind(),
+    playformCompress(),
+    pagefind(),
+    partytown(),
+  ],
   markdown: {
     shikiConfig: {
       themes: {
-        light: 'github-light',
-        dark: 'github-dark',
+        light: "github-light",
+        dark: "github-dark",
       },
       transformers: [
         {
@@ -31,51 +40,51 @@ export default defineConfig({
             return code;
           },
           root(node) {
-            if (node.tagName === 'pre') {
-              node.tagName = 'figure';
-              node.properties.className = ['highlight', this.lang];
+            if (node.tagName === "pre") {
+              node.tagName = "figure";
+              node.properties.className = ["highlight", this.lang];
             }
           },
           pre(node) {
             const toolsDiv = {
-              type: 'element',
-              tagName: 'div',
-              properties: { className: ['highlight-tools'] },
+              type: "element",
+              tagName: "div",
+              properties: { className: ["highlight-tools"] },
               children: [
                 {
-                  type: 'element',
-                  tagName: 'div',
-                  properties: { className: ['code-lang'] },
-                  children: [{ type: 'text', value: this.lang.toUpperCase() }],
+                  type: "element",
+                  tagName: "div",
+                  properties: { className: ["code-lang"] },
+                  children: [{ type: "text", value: this.lang.toUpperCase() }],
                 },
               ],
             };
             const lineNumberCode = {
-              type: 'element',
-              tagName: 'code',
+              type: "element",
+              tagName: "code",
               children: [],
             };
             const lineNumberPre = {
-              type: 'element',
-              tagName: 'pre',
-              properties: { className: ['frosti-code', 'gutter'] },
+              type: "element",
+              tagName: "pre",
+              properties: { className: ["frosti-code", "gutter"] },
               children: [lineNumberCode],
             };
             const codeContentPre = {
-              type: 'element',
-              tagName: 'pre',
-              properties: { className: ['frosti-code', 'code'] },
+              type: "element",
+              tagName: "pre",
+              properties: { className: ["frosti-code", "code"] },
               children: [],
             };
             node.children.forEach((lineNode, index, count) => {
               count = 0;
               lineNode.children.forEach(() => {
-                if (count & 1 === 1) {
+                if (count & (1 === 1)) {
                   lineNumberCode.children.push({
-                    type: 'element',
-                    tagName: 'div',
-                    properties: { className: ['line'] },
-                    children: [{ type: 'text', value: String(index + 1) }],
+                    type: "element",
+                    tagName: "div",
+                    properties: { className: ["line"] },
+                    children: [{ type: "text", value: String(index + 1) }],
                   });
                   index++;
                 }
@@ -85,15 +94,15 @@ export default defineConfig({
               codeContentPre.children.push(lineNode);
             });
             const table = {
-              type: 'element',
-              tagName: 'div',
-              properties: { className: ['highlight-code'] },
+              type: "element",
+              tagName: "div",
+              properties: { className: ["highlight-code"] },
               children: [lineNumberPre, codeContentPre],
             };
             return {
-              type: 'element',
-              tagName: 'figure',
-              properties: { className: ['highlight', this.lang] },
+              type: "element",
+              tagName: "figure",
+              properties: { className: ["highlight", this.lang] },
               children: [toolsDiv, table],
             };
           },
@@ -101,6 +110,6 @@ export default defineConfig({
       ],
     },
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [rehypeKatex],
   },
 });
